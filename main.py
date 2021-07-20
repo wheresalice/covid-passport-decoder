839e337b0b448a4cc62404cd7a909a0541580fbc
wheresalice <wheresalice@users.noreply.github.com>
2021-07-20 19:37:02 +0100

    feat: strip out the headers and handle timestamps

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import base45
 import zlib
 from cose.messages import CoseMessage
 import cbor
+import json
+from datetime import datetime
 
 qr_code = sys.argv[1]
 
@@ -19,4 +21,20 @@ cose_decoded = CoseMessage.decode(zlib_decompressed)
 
 cbor_payload = cbor.loads(cose_decoded.payload)
 
-print(cbor_payload)
+claim_names = {1: "Issuer", 6: "Issued At", 4: "Expiration time", -260: "Health claims"}
+for k in cbor_payload:
+    if k != -260:
+        n = f'Claim {k} (unknown)'
+        if k in claim_names:
+            n = claim_names[k]
+        if k == 6 or k == 4:
+            ts = int(cbor_payload[k])
+            print(f'{n:20}: {datetime.utcfromtimestamp(ts).strftime("%Y-%m-%d %H:%M:%S")}')
+        else:
+            print(f'{n:20}: {cbor_payload[k]}')
+
+payload = cbor_payload[-260][1]
+n = 'Health payload'
+print(f'{n:20}: ', end="")
+
+print(json.dumps(payload))
